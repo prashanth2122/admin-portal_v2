c9247123b0c2abb4c66f7fdd1ff173b9052deffd
prashanth2122 <82014342+prashanth2122@users.noreply.github.com>
2023-07-31 14:46:00 +0530

    algos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-portal_v2';
+
+  ngOnInit() {
+    let str = 'prashanth reddy'
+    console.log(this.countOccString(str));
+  }
+  countOccString(str: string) {
+    var charCount: any = {}
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i]
+      if (charCount[char]) {
+        charCount[char]++
+      }
+      else {
+        charCount[char] = 1
+      }
+    }
+    return charCount;
+  }
 }
